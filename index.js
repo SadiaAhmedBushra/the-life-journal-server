dc40ccb8936d67adbe76cc2d68f747abba538488
Sadia Ahmed Bushra <sadiahmedbushra28@gmail.com>
2025-12-08 21:08:53 +0600

    filtered lessons

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lpz93gz.mongodb.net/?appName=Cluster0`;
 
@@ -17,9 +17,8 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
@@ -29,21 +28,43 @@ async function run() {
     const lessonCollection = db.collection("lessons");
 
     // lesson api
-    app.get('/lessons', async (req, res) => {
+    app.get("/lessons", async (req, res) => {
+      try {
+        const query = {};
+        const { email } = req.query;
+        console.log("QUERY EMAIL:", email);
 
+        if (email) {
+          query.email = email;
+        }
+
+        // all lessons
+        const allLessons = await lessonCollection.find({}).toArray();
+        console.log("All Lessons:", allLessons);
+
+        // filtered lessons
+        const result = await lessonCollection.find(query).toArray();
+        console.log("Lessons Filtered by User Email:", result);
+
+        res.send(result); 
+      } catch (error) {
+        console.error("Error fetching lessons:", error);
+        res.status(500).send({ error: "Failed to fetch lessons" });
+      }
     });
 
-    app.post('/lessons', async (req, res) => {
+    app.post("/lessons", async (req, res) => {
       const lesson = req.body;
       console.log(lesson);
       const result = await lessonCollection.insertOne(lesson);
       res.send(result);
     });
 
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -51,10 +72,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to The Life Journal!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to The Life Journal!");
+});
 
 app.listen(port, () => {
-  console.log(`The Life Journal is listening on port ${port}`)
-})
+  console.log(`The Life Journal is listening on port ${port}`);
+});
